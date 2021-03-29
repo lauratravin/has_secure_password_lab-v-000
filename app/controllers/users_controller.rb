a5e28ec754a857a8457a2195d068239b98c8be81
Laura Messa Travin <laura.travin@gmail.com>
2021-03-29 22:57:00 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,17 @@ class UsersController < ApplicationController
   def new
   end
   def create
+
   end
+
+
+
+  private
+
+   def user_params
+
+     params.require(:user).permit(:name, :password, :password_confirmation)
+
+   end
+
 end
