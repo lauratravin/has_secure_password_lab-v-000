f7a85675bd274b9a5bdc3d7cd8aeb24a5f91dd59
Laura Messa Travin <laura.travin@gmail.com>
2021-03-29 23:07:30 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,18 @@ class UsersController < ApplicationController
   def new
   end
   def create
-
+     @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to :welcome
+      else
+        redirect_to :new_user
+      end
+      #another way
+      # @user = User.create(user_params)
+      #  return redirect_to controller: 'users', action: 'new' unless @user.save
+      #  session[:user_id] = @user.id
+      #  redirect_to controller: 'welcome', action: 'home'
   end
 
 
