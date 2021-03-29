cf68d1c05bdfd869eabc4121d0e58794d18e039d
Laura Messa Travin <laura.travin@gmail.com>
2021-03-29 23:15:45 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
      @user = User.new(user_params)
       if @user.save
         session[:user_id] = @user.id
-        redirect_to :welcome
+        redirect_to '/'
       else
         redirect_to :new_user
       end
